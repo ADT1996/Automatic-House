87084b9282e9d8ec7925a3e3ea4cda8015b525a4
ADT1996 <autuyen199633@gmail.com>
2018-12-14 11:51:43 +0700

    Add Web

---
+++ WebApplication2/WebApplication2/Controllers/MotionAlarmController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class MotionAlarmController : Controller
+    {
+        //GET: MotionAlarm
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        static string alarm;
+        public ActionResult ControllAlarm()
+        {
+          ViewData["alarm"] = alarm;
+            return View();
+        }
+
+        public string setAlarm(string _alarm)
+        {
+            if ("Auto".Equals(_alarm))
+            {
+                alarm = "1";
+            }
+            else if ("Turn On".Equals(_alarm))
+            {
+                alarm = "2";
+            }
+            else
+            {
+                alarm = "3";
+            }
+            return "OK";
+        }
+
+        public string demo()
+        {
+            return alarm;
+        }
+    }
+}
